0e18eef59283dcfc5a2b710dc0ce3b3912e84016
lizhongyong0570 <144329891+lizhongyong0570@users.noreply.github.com>
2023-09-10 11:18:37 +0800

    Update diy-part2.sh

--- config/Lienol-master/diy-part2.sh
+++ config/Lienol-master/diy-part2.sh
@@ -23,8 +23,8 @@ echo "DISTRIB_SOURCECODE='Lienol'" >>package/base-files/files/etc/openwrt_releas
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
+#svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/xiaorouji/openwrt-passwall.git  package/luci-app-passwall
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
 # svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
